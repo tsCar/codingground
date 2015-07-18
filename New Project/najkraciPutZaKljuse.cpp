fe42365ebfc09eb3a1cac0660c3ec62bada22f8b
tsCar <drugijebuatisemuzaferekupaj@hotmail.com>
2015-07-18 19:01:47 +0200

    Update najkraciPutZaKljuse.cpp

--- New Project/najkraciPutZaKljuse.cpp
+++ New Project/najkraciPutZaKljuse.cpp
@@ -4,29 +4,42 @@
 
 using namespace std;
 int level=0;//za debug, pobrisat kasnije
-const int M=4;
-const int N=3;
-int racunaj(int[][N],int,int);
-int rekurzivno(int polje[][N],int M, int N,int px, int py, int potezi[][2]);
+const int M=3;
+const int N=4;
+int racunaj(int**,int,int);
+int rekurzivno(int **,int M, int N,int px, int py, int potezi[][2]);
 int main()
 {
-   int polje[M][N];
-   for (int i=0;i<M;i++){
-       for (int j=0;j<N;j++){
+
+ int **polje=new int*[N];
+ 
+   for (int i=0;i<N;i++){
+       polje[i]=new int[M];
+       for (int j=0;j<M;j++){
            polje[i][j]=0;
        }
    }
    polje[2][0]=1; //Neka polja su blokirana
-   polje[1][2]=1;
+   polje[1][2]=1;  
+   
+   for (int i=0;i<N;i++){
+       for (int j=0;j<M;j++){
+           cout<<polje[i][j];
+       }cout<<endl;
+   }
    cout<<"\n\n\n\n\n\n\n\n\n\n\n";
-   cout<<"Minimalan broj poteza je "<<racunaj(polje,M,N)<<endl;
+   cout<<"Minimalan broj poteza je "<<racunaj(polje,N,M)<<endl;
    return 0;
 }
-int rekurzivno(int polje[][N],int M, int N,int px, int py, int potezi[][2]){
+int rekurzivno(int **polje,int M, int N,int px, int py, int potezi[][2]){
     cout<<"nova rekurzija ("<<px<<","<<py<<")\n";
     int tmpMin[8],min;
     if(px>=M||py>=N||px<0||py<0) {cout<<"vracam -1 za ("<<px<<","<<py<<") jer je izvan table\n";return -1;}
-    if(polje[px][py]==1){cout<<"vracam -1 za ("<<px<<","<<py<<") jer je posjeceno\n";return -1;}
+    if(polje[px][py]==1){
+        cout<<"vracam -1 za ("<<px<<","<<py<<") jer je posjeceno\n";
+        return -1;
+        
+    }
     if(px==M-1&&py==N-1){cout<<"vracam 1 za ("<<px<<","<<py<<")\n";return 0;}
     else{
         level++;
@@ -51,11 +64,10 @@ int rekurzivno(int polje[][N],int M, int N,int px, int py, int potezi[][2]){
         else {cout<<"vracam -1\n";return -1;}
     }
 }
-int racunaj(int polje[][N],int M,int N){
+int racunaj(int **polje,int M,int N){
     int min;
     int potezi[8][2]={{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2},{2,-1}};
     min=rekurzivno(polje,M,N,0,0,potezi);
     cout<<"kad se vrati iz rekurzija min je "<<min<<endl;
     return min;
 }
-
